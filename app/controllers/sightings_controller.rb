93f62b696a47a39365a5cabe75efb053c673739f
osha7 <ogroetz@gmail.com>
2020-07-23 16:24:29 -0600

    Done.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,13 @@
+class SightingsController < ApplicationController
+
+    def index
+        sightings = Sighting.all
+        render json: sightings, include: [:bird, :location]
+      end
+
+    def show
+        sighting = Sighting.find_by(id: params[:id])
+        render json: sighting, include: [:bird, :location], except: [:updated_at]
+    end
+
+end
